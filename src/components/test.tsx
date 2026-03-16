353f9e2e77849fe027f8a1d2dff4c7bcddd38f88
Chris Tang <1073412@hr.nl>
2026-03-16 11:00:54 +0100

    ADD nieuwe component, om alle andere componenten in te laden, om de sticky scroll effect in te laden

---
+++ src/components/test.tsx
@@ -0,0 +1,15 @@
+import AboutUs from "./aboutUs";
+import TeamValues from "./teamValues";
+import Sprint0 from "./sprint-0";
+
+const Test = () => {
+  return (
+    <>
+      <AboutUs />
+      <TeamValues />
+      <Sprint0 />
+    </>
+  );
+};
+
+export default Test;
